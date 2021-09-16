7319c0298c863dcd6b794614d0dc6154b2ea0619
Admin <Admin@DESKTOP-FHNDOQS>
2021-09-16 09:36:03 +0530

    hello

---
+++ automationqsp/src/qsp/LaunchFirefoxBrowser.java
@@ -0,0 +1,18 @@
+package qsp;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class LaunchFirefoxBrowser 
+{
+    static 
+    {
+    	System.setProperty("webdriver.gecko.driver","./drivers/geckodriver.exe");
+    }
+	public static void main(String[] args) 
+	{
+
+		new FirefoxDriver();
+
+	}
+
+}
